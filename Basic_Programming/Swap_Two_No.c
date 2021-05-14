bdfb8e14b5f7c453599b4be8aa6d8990553f5aff
Kanishk Tyagi <75319323+Kanishk0412@users.noreply.github.com>
2021-05-14 11:23:34 +0530

    reason

--- Basic_Programming/Swap_Two_No.c
+++ Basic_Programming/Swap_Two_No.c
@@ -2,5 +2,14 @@
 int main()
 {
     int a, b, temp;
-    
+    printf("Enter the Number:");
+    scanf("%d",&a);
+    printf("\nEnter the Second Number:");
+    scanf("%d",&b);
+    temp=a;
+    a=b;
+    b=temp;
+    printf("\nFirst Number after Swapping %d",a);
+    printf("\nSecond Number after Swapping %d",b);
+    return 0;
 }
--- Conditional_Operators/Leap_Year.c
+++ Conditional_Operators/Leap_Year.c
@@ -2,7 +2,7 @@
 int main()
 {
     int year;
-    printf("Enter a year \n");
+    printf("Enter a year\n");
     scanf("%d", &year);
     (year%4==0 && year%100!=0) ? printf("Leap Year") : (year%400==0 ) ? printf("Leap Year") : printf("not a leap year");
     return 0;
